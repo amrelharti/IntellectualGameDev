3b846b87444c4f852eef8324d9cef4921ffe584a
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-07-17 16:44:02 +0100

    initial commit

---
+++ src/main/java/com/example/intellectual_game/IntellectualGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.intellectual_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectualGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellectualGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intellectual_game/IntellectualGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intellectual_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectualGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
