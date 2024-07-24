8cad797a8edb7e434424907d5e90f43294b4c5d6
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-07-24 16:57:28 +0100

    log in method for now

--- src/main/java/com/example/intellectual_game/Repo/PlayerRepo.java
+++ src/main/java/com/example/intellectual_game/Repo/PlayerRepo.java
@@ -4,6 +4,10 @@ import com.example.intellectual_game.Entities.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepo extends MongoRepository<Player, String> {
+    Optional<Player> findByUsernameAndPassword(String username, String password);
+    Optional<Player> findByUsername(String username);
 }
