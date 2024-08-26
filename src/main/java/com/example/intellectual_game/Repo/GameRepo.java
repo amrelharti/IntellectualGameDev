2aaae04e6b120326311974dee2fdf9ad79c9450f
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-08-26 17:59:22 +0100

    back end config 'WS, Game'

--- src/main/java/com/example/intellectual_game/Repo/GameRepo.java
+++ src/main/java/com/example/intellectual_game/Repo/GameRepo.java
@@ -1,9 +1,15 @@
 package com.example.intellectual_game.Repo;
 
 import com.example.intellectual_game.Entities.Game;
+import com.example.intellectual_game.enums.GameState;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepo extends MongoRepository<Game, String> {
+    Game findFirstByState(GameState state);
+
+    List<Game> findByState(GameState gameState);
 }
